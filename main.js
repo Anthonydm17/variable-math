cd19fdb4cde50160e7620de183e8196a9b797cee
Anthony Martinez <straff@anthonys-mbp.myfiosgateway.com>
2022-02-03 15:55:47 -0500

    added main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+let age = 27;
+let future = 5;
+let sum = age + future;
+let num1 = 12;
+let num2 = 4;
+let together = num1 + num2;
+let name = "Anthony";
+let five = 5;
+let seven = 7;
+let div = seven % five;
+let x1 = 2;
+let y1 = 4;
+let x2 = 6;
+let y2 = 8; 
+let para = 6 - 2;
+let para2 = 8 - 4;
+let exp1 = 4 * 4
+let exp2 = 4 * 4
+let add = 16 + 16 
+let final = Math.sqrt(32);
+
+console.log("Anthony is " + age + "in" + future + "years, he will be" +sum)
+console.log(+ num1 + "and" + num2 + "add up to" +together)
+console.log("Hey! Hows it going" +name)
+console.log( + five + "%" + seven + "=" + div)
+
+console.log("subtracting x2 and x1 is" + para + "and subtracting y2 and y 1 is" + para2 + ".4 to the second power is" + exp1 + "and 4 to the second power is" + exp2 +
+"add them together to get" + add + ".when you take the square root of this, it gives you the final distance of" + final)
